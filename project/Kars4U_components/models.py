fb23931642b1f20af3526085d49c4a104f5c34b0
daniel-brady <Dennison04~~>
2022-03-03 21:48:01 -0500

    connected to database

--- project/Kars4U_components/models.py
+++ project/Kars4U_components/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    car_id = models.IntegerField( primary_key=True)
+    license_plate_number = models.CharField(max_length=10)
+    state = models.CharField(max_length=25)
+    make = models.CharField(max_length=25)
+    model = models.CharField(max_length=25)
+    color = models.CharField(max_length=25)
+    car_type = models.CharField(max_length=25)
+    is_available = models.BooleanField(max_length=10)
+
+
+class Employee(models.Model):
+    employee_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=25)
+    store_id = models.IntegerField()
